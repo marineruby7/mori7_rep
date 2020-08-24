fa1e4247912ab84e3db979a74abce4be06c5c0e2
marineruby7 <70142515+marineruby7@users.noreply.github.com>
2020-08-24 20:54:44 +0900

    Create sample.c
    First Add/Commit

---
+++ sample.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main()
+{
+    unsigned int x, y;
+    unsigned int z;
+
+    x = 5;
+    y = 0;
+    z = x;
+
+    for(int i=0; i<x; i++)
+    {
+        printf("Hello World\n",(x/y));
+    }
+}
